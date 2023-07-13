1c7eaf2a09260b23f4d520f1567c862863c7b154
Luzan99 <luzanmaharjan@gmail.com>
2023-07-13 12:15:47 +0545

    test

---
+++ app/src/main/java/com/example/dietmonitoring/DescriptionActivity.java
@@ -0,0 +1,15 @@
+package com.example.dietmonitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.description);
+
+    }
+}
---
+++ app/src/main/java/com/example/dietmonitoring/ModifyFoodsActivity.java
@@ -0,0 +1,14 @@
+package com.example.dietmonitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyFoodsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modify_foods);
+    }
+}
